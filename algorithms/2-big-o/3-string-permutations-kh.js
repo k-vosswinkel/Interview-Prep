720f33e9cb577469104d33b4e33a818f88e8c08f
Kait Hoehne <kait.hoehne@gmail.com>
2018-05-16 17:45:48 -0400

    looking at string permutations again

--- algorithms/2-big-o/3-string-permutations-kh.js
+++ algorithms/2-big-o/3-string-permutations-kh.js
@@ -1,10 +1,16 @@
-let stringPermutations(str){
-  let results = []
+let stringPermutations = (str) => {
+  let results = [];
 
   for (let i = 0; i < str.length; i++){
-
+    let currLetter = str[i]
+    let currResults = ''
+    for (let n = 0; n < str.length; n++){
+      if (currLetter !== str[n]) currResults += str[n]
+    }
+    results.push(currResults)
+    currResults = ''
   }
-  //nested loops!
+  console.log(results)
 }
 
 //Try Philip's way here - slicing and recursing
