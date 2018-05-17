655bae9c02d0d99082676a53d4be8467c9224682
Kait Hoehne <kait.hoehne@gmail.com>
2018-05-17 17:55:17 -0400

    added Philips permutation solution

--- algorithms/2-big-o/3-string-permutations-kh.js
+++ algorithms/2-big-o/3-string-permutations-kh.js
@@ -14,3 +14,25 @@ let stringPermutations = (str) => {
 }
 
 //Try Philip's way here - slicing and recursing
+function permutations(array) {
+
+  let res = []
+
+  console.log(array)
+
+  if (array.length === 1) return [array]
+
+  for (let idx = 0; idx < array.length; idx++) {
+
+    let branchMember = array[idx]
+    let virtualArray = array.slice(0, idx).concat(array.slice(idx + 1))
+
+    permutations(virtualArray).forEach(eachPermutation => {
+      res.push([branchMember, ...eachPermutation])
+    })
+  }
+
+  console.log('Lets return ', res)
+  return res
+
+}
