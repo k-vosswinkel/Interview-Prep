1a9abfdeaafdd5a628983ea75672641864f89d2d
Kait Hoehne <kait.hoehne@gmail.com>
2018-06-18 11:06:00 -0400

    cleaned up and edited word count engine

--- algorithms/4-optimizations/word-count-engine.js
+++ algorithms/4-optimizations/word-count-engine.js
@@ -1,13 +1,20 @@
-// Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all unique words in it and their number of occurrences, sorted by the number of occurrences in a descending order.If two or more words have the same count, they should be sorted according to their order in the original sentence.Assume that all letters are in english alphabet.You function should be case -insensitive, so for instance, the words “Perfect” and “perfect” should be considered the same word.
+// Implement a document scanning function wordCountEngine, which receives a string document and returns a list of all unique words in it and their number of occurrences, sorted by the number of occurrences in a descending order.
+
+// If two or more words have the same count, they should be sorted according to their order in the original sentence.
+
+// Assume that all letters are in english alphabet. Your function should be case-insensitive, so for instance, the words “Perfect” and “perfect” should be considered the same word.
 
 // The engine should strip out punctuation(even in the middle of a word) and use whitespaces to separate words.
-// Analyze the time and space complexities of your solution.Try to optimize for time while keeping a polynomial space complexity.
+
+// Analyze the time and space complexities of your solution. Try to optimize for time while keeping a polynomial space complexity.
 
 function wordCountEngine(document) {
   //turning everything to lowercase
   let lowercaseDoc = document.toLowerCase();
+
   //strip out all exteraneous characters
   let onlyLetters = lowercaseDoc.replace(/[!@#$%^&*.,]/g, "");
+
   //string split using whitespace
   let wordArr = onlyLetters.split(' ');
   let dictionary = new Map();
