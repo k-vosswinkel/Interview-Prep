93b3dd6b98948bb9458d4027bd5b18835fd50f47
Kait Hoehne <kait.hoehne@gmail.com>
2018-07-03 12:46:09 -0400

    added math-y solution

--- algorithms/8-sorting/quick-sort/solution.js
+++ algorithms/8-sorting/quick-sort/solution.js
@@ -1,5 +1,4 @@
 //Quicksort implementation
-'use strict'
 function quickSort(arr){
 
     (function quickSortImplement(leftPointer, rightPointer, pivotIndex){
@@ -11,16 +10,19 @@ function quickSort(arr){
                 pivotIndex = i;
             }
         }
-        
+
         //note, using a random pivot point to prevent worst-case behavior
         if(pivotIndex > leftPointer) quickSortImplement(0, pivotIndex, Math.floor(Math.random()*(pivotIndex - leftPointer)));
         if(pivotIndex < rightPointer) quickSortImplement(pivotIndex+1, rightPointer, Math.floor(Math.random()*(rightPointer - pivotIndex))+pivotIndex+1);
-        
+
     })(0, arr.length - 1, Math.floor(Math.random()*arr.length));
 
     return arr;
 }
 
 var arr = [5,2,4,1,3,7,9,8];
+
+function quickSort(arr) {
+}
 
 quickSort(arr);
